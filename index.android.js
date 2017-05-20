430ae14b002ef22fe7dc55f6ed68b7ad7d4cdcc3
Eosin Chen <eosinchen@gmail.com>
2017-05-20 16:16:15 +0800

    Branch 1 Stage 1

--- index.android.js
+++ index.android.js
@@ -25,6 +25,7 @@ export default class GitSample extends Component {
         <Text style={styles.instructions}>
           Shake or press menu button for dev menu - 12345
           ABC
+          分支 Branch 1
           第二次修改
           03:45
         </Text>
